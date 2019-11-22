1afb1cb3605bdcbbd54fd21ac6d089ae87309a93
Keira Hartley <khartley@lcs.on.ca>
2019-11-22 15:22:49 -0500

    I wrote code to understand the difference between doubles and floats.

---
+++ SwiftApprentice/Chapter1.playground/Pages/Challenges.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+// Chapter 1 Challenges
+
+// Challenge 1 - Chapter 1
+
+let age = 16
+var dogs = 2
+dogs = dogs + 1
+
+// Challenge 10 - Chapter 2
+
+let DP = Double.pi
+
+let FP = Float.pi
+
+print ("Double keeps all of the decimals, but float only keeps 6 of the decimals of Pi.")
