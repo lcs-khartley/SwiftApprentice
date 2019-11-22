829580707e92c0785a751c84309846fafd59adca
Keira Hartley <khartley@lcs.on.ca>
2019-11-22 14:33:29 -0500

    Initial Commit

---
+++ SwiftApprentice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftApprentice
+//
+//  Created by Hartley, Keira on 2019-11-22.
+//  Copyright © 2019 Hartley, Keira. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
